a9ef61f455a4fb5ee77c1d7381b1fc767b28d2ad
bluebigthink <bluebigthink@gmail.com>
2025-01-30 21:34:57 -0500

    add more

--- src/app/(main)/websites/WebsitesPage.tsx
+++ src/app/(main)/websites/WebsitesPage.tsx
@@ -1,10 +1,15 @@
 'use client';
+import { useEffect } from 'react';
 import WebsitesHeader from 'app/(main)/settings/websites/WebsitesHeader';
 import WebsitesDataTable from 'app/(main)/settings/websites/WebsitesDataTable';
 import { useTeamUrl } from 'components/hooks';
-
+import { notifyEvent } from './[websiteId]/realtime/reporter';
 export default function WebsitesPage() {
   const { teamId } = useTeamUrl();
+
+  useEffect(() => {
+    notifyEvent('website');
+  }, []);
 
   return (
     <>
